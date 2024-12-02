9da7ebc28ac7e726b2f45c7257be24f11d1087df
SERENA FERRARO <serenaferraro21@gmail.com>
2024-12-02 23:08:56 +0100

    state update

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -2,32 +2,50 @@ import { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 
 class MyForm extends Component {
-    render() {
-        const today = new Date().toISOString().split('T')[0];
-        return (
-            <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Task:</Form.Label>
-              <Form.Control type="email" placeholder="Remember to.." />
-              {/* <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text> */}
-            </Form.Group>
-      
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="date" placeholder="When?" min={today}/>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Mark as important" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-            Save
-            </Button>
-          </Form>
-        );
-        
-      }
+    state ={
+        toDo:{
+            task: '',
+            date: '',
+            important: false
+        }
     }
+  render() {
+    const today = new Date().toISOString().split("T")[0];
+    const { task, date, important } = this.state.toDo;
+    return (
+      <Form>
+        <Form.Group className="mb-3" >
+          <Form.Label>Task:</Form.Label>
+          <Form.Control type="text" placeholder="Remember to.." 
+          value={task}
+          onChange={(e)=>{this.setState({toDo:{...this.state.toDo,
+            task:e.target.value
+          }})}}/>
+        </Form.Group>
 
-    export default MyForm
+        <Form.Group className="mb-3" >
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="date" placeholder="When?" min={today}
+          value={date} 
+          onChange={(e)=>{this.setState({toDo:{...this.state.toDo,
+            date:e.target.value
+          }})}}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Check type="checkbox" label="Mark as important"
+          checked={important} 
+          onChange={(e)=>{this.setState({toDo:{...this.state.toDo,
+            important:e.target.checked
+          }})}}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Save
+        </Button>
+      </Form>
+    );
+  }
+}
+
+export default MyForm;
